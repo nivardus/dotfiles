09ea6365cd0b2bb8f268c976f9f63b2f7cb7e201
Bennett Goble <nivardus@gmail.com>
2015-01-01 11:12:45 -0500

    Make rbenv plugin dir

--- install.sh
+++ install.sh
@@ -9,13 +9,14 @@ else
   return 1
 fi
 
-# Install oh my zsh, rbenv, ruby build, nvm 
+# oh my zsh, rbenv, ruby build, nvm 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+mkdir ~/.rbenv/plugins
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
 
-# Install pathogen.vim
+# pathogen.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
