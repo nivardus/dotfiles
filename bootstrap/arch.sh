c0aba4b5994550c03b05172b8263716bf41e5391
Bennett Goble <nivardus@gmail.com>
2015-05-23 11:53:48 -0400

    Arch Linux switched from packer to aura

--- bootstrap/arch.sh
+++ bootstrap/arch.sh
@@ -1,26 +1,26 @@
 echo "Running Arch Linux bootstrap"
 
-PACKER_TARBALL="packer.tar.gz"
-PACKER_TMP_DIR="/tmp/packer-build"
-PACKER_URL="https://aur.archlinux.org/packages/pa/packer/$PACKER_TARBALL"
+AURA_TARBALL="aura-bin.tar.gz"
+AURA_TMP_DIR="/tmp/aura-bin-build"
+AURA_URL="https://aur.archlinux.org/packages/pa/aura-bin/$PACKER_TARBALL"
 
 sudo pacman -Sy
-sudo pacman -S git ruby zsh nodejs vim curl expac jshon makepkg --noconfirm --needed
+sudo pacman -S git ruby zsh nodejs vim curl abs gmp pcre --noconfirm --needed
 
 # Install packer
-if ! type "packer" > /dev/null; then
+if ! type "aura" > /dev/null; then
 (
-mkdir $PACKER_TMP_DIR
-cd $PACKER_TMP_DIR
-curl -Ss $PACKER_URL
-tar -xvf $PACKER_TARBALL
-cd packer
+mkdir $AURA_TMP_DIR
+cd $AURA_TMP_DIR
+curl -Ss $AURA_URL
+tar -xvf $AURA_TARBALL
+cd aura-bin
 makepkg -i
 cd $HOME
-rm -rf $PACKER_TMP_DIR
+rm -rf $AURA_TMP_DIR
 )
 fi
 
-packer -S rcm-git --noconfirm --noedit
+sudo aura -A rcm-git --noconfirm
 
 chsh -s $(which zsh)
